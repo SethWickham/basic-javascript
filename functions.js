f59a9c469e2283d28f939f03c6d751d894d28c97
SethWickham <seth.wickham@hotmail.com>
2020-01-27 20:08:16 -0800

    created basicFunctions.js file with addThree func and comments/ console log

---
+++ functions.js
@@ -0,0 +1,10 @@
+//run code: node functions.js
+
+//declaring function
+// inside of parens are parameters
+function addingThree(a, b, c) {
+  //function body inside of the curly brackets
+  return a + b + c;
+}
+//console log: calling the function and passing the arguments: 1, 2, 4
+console.log(addingThree(1, 2, 4));
